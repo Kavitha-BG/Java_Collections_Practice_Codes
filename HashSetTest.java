62ae81f071fa59488d15126ebef3830ceb19c67f
Kavitha B G <51849346+Kavitha-BG@users.noreply.github.com>
2022-07-26 18:42:04 +0530

    Add files via upload

---
+++ HashSetTest.java
@@ -0,0 +1,21 @@
+package com.collections;
+
+import java.util.HashSet;
+
+public class HashSetTest {
+
+	public static void main(String[] args) {
+		
+		HashSet hs = new HashSet();
+		hs.add(120);
+		hs.add(60);
+		hs.add(160);
+		hs.add(40);
+		hs.add(80);
+		hs.add(140);
+		hs.add(180);
+		System.out.println(hs);
+		
+	}
+
+}
